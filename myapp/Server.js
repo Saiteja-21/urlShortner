efd15bddf14a1290c903bfa2b79f8247af1d2214
Saiteja-21 <saitejakatkam123@gmail.com>
2023-05-29 16:41:47 +0530

    step2,step3

--- myapp/Server.js
+++ myapp/Server.js
@@ -8,6 +8,32 @@ const port=process.env.PORT||3000;
 app.get('/',(req,res)=>{
     res.send('welcome to url shortner')
 })
+app.get('/i/google',(req,res)=>{
+    res.send('https://www.google.com')
+})
+app.get('/i/github',(req,res)=>{
+    res.send('https://github.com')
+})
+app.get('/i',(req,res)=>{
+    const obj=[{
+       endpoint:'/i/google',
+       returns:"https://www.google.com",
+      
+    },
+{
+    endpoint:'/i/github',
+   returns :"https://github.com"
+}]
+    res.json(obj)
+
+})
+app.get('/r/google',(req,res)=>{
+    res.redirect('https://www.google.com')
+})
+app.get('/r/github',(req,res)=>{
+    res.redirect('https://www.github.com')
+})
+
 app.listen(port,()=>{
     console.log('server is listening at port 5000')
 })
